25882d615bed47657fda91fe764ea5bcac22d25a
Ali Semih Ural <60106172+alisemihural@users.noreply.github.com>
2024-05-14 23:57:21 -0400

    Setup 955.Delete Columns to Make Sorted II

---
+++ 955_M_DeleteColumnsToMakeSortedII/delete_columns_to_make_sorted_ii_solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minDeletionSize(self, strs: List[str]) -> int:
+        pass
+
+if __name__ == '__main__':
+    strs = ["ca","bb","ac"]
+    solution = Solution()
+    print(solution.minDeletionSize(strs))
